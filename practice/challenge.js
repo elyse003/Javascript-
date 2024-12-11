6e43becc62560fd9c5658d1f9f8e1542c18e2a1c
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-11 15:23:32 +0200

    challenge 2

--- practice/challenge.js
+++ practice/challenge.js
@@ -13,3 +13,16 @@ console.log(myFunction(null));
 console.log(myFunction('string'));
 console.log(myFunction(['array']));
 }
+function
+myFunction
+(
+a, n
+)
+{
+
+return a[n-1];
+console.log(myFunction('abcd',1));
+console.log(myFunction('zyxbwol',5));
+console.log(myFunction('gfedcba',3));
+
+}
