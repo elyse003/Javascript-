4569afe5e6dc5ddb915246bb785e460e2e1c4586
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-11 15:56:19 +0200

    find the position of one string in an other

--- practice/challenge.js
+++ practice/challenge.js
@@ -62,3 +62,16 @@ console.log(myFunction('abcdefg'));
 console.log(myFunction('1234'));
 console.log(myFunction('fgedcba'));
 }
+
+function
+myFunction
+(
+a
+)
+{
+
+return a.indexOf("is");
+console.log(myFunction("praise"));
+console.log(myFunction("risky"));
+console.log(myFunction("paris"));
+}
