3d8ae38b42ff36baa51e6763b98961e05e4d0da6
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-11 15:09:00 +0200

    practice folder

---
+++ Day1/practice/challenge.js
@@ -0,0 +1,15 @@
+function
+myFunction
+(
+a
+)
+{
+
+return typeof a;
+console.log(myFunction(1));
+console.log(myFunction(false);
+console.log(myFunction({});
+console.log(myFunction(null));
+console.log(myFunction('string');
+console.log(myFunction(['array']);
+}
