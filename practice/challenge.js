b48608d90b3fb6666c1014c5159af24d5c32508f
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-11 15:45:48 +0200

    get first n characters of a string

--- practice/challenge.js
+++ practice/challenge.js
@@ -50,3 +50,15 @@ console.log(myFunction('abcdefg'));
 console.log(myFunction('1234'));
 console.log(myFunction('fgedcba'));
 }
+function
+myFunction
+(
+a
+)
+{
+
+return a.slice(0,3);
+console.log(myFunction('abcdefg'));
+console.log(myFunction('1234'));
+console.log(myFunction('fgedcba'));
+}
