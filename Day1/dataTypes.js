a1c5011ff67f88a1fa45667fac660cef86923768
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-11 15:05:10 +0200

    conditions js

---
+++ Day1/dataTypes.js
@@ -0,0 +1,18 @@
+//BOOLEAN
+
+//String
+//number
+let age = 19;
+let name = "123";
+//null
+let symbol = null;
+//undefined
+
+//CONDITIONS
+
+let age = 16;
+if(age < 18){
+    console.log('ypo are allowed to drink')
+}else{
+    console.log(you are not allowed to drink)
+}
--- Day1/variables.js
+++ Day1/variables.js
@@ -4,3 +4,30 @@ let b = 10;
 console.log(a+b);
 
 console.log(myName);
+
+let age = 20;
+if(age < 18){
+    console.log('yuo are allowed to drink')
+}else{
+    console.log('you are not allowed to drink')
+}
+
+let BankAccount = 20;
+
+if(BankAccount >= 1000000){
+    console.log('you rich🤯')
+}else if(BankAccount == 500000){
+    console.log('you good😁')
+}else {
+    console.log('you broke😓')
+}
+
+let country = 'France';
+if(country == 'France'){
+    console.log('Bonjour')
+
+}else if(country == Tanzania){
+    console.log('Shikamo')
+}else{
+    console.log('Hello')
+}
