5fce00d08e6f7bc8cd7b43811960c6332e8a8ffb
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-12 11:33:39 +0200

    comparison operators, strict equality

--- practice/challenge.js
+++ practice/challenge.js
@@ -14,6 +14,20 @@ console.log(myFunction(99, 1));
 function
 myFunction
 (
+a, b
+)
+{
+
+return a === b;
+console.log(myFunction(2, 3));
+console.log(myFunction(3, 3));
+console.log(myFunction(1, '1'));
+console.log(myFunction('10', '10'));
+
+}
+function
+myFunction
+(
 a
 )
 {
