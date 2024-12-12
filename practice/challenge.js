0e40b1fd3b83833ebd844840f087c300387b6109
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-12 10:43:51 +0200

    how many characters does a character occur

--- practice/challenge.js
+++ practice/challenge.js
@@ -154,3 +154,17 @@ console.log(myFunction(10));
 console.log(myFunction(-4));
 console.log(myFunction(5));
 }
+function
+myFunction
+(
+a, b
+)
+{
+
+return  b.split(a).length - 1;
+
+console.log(myFunction('m', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('h', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('?', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('z', 'how many times does the character occur in this sentence?'));
+}
