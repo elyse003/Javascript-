a1865a9cb9b8a2a35837d65786a565905d4f1bb7
elyse003 <155962493+elyse003@users.noreply.github.com>
2024-12-13 10:12:37 +0200

    prework

---
+++ Day2/prework.js
@@ -0,0 +1,7 @@
+let age = 26;
+if(age < 13){
+  console.log(' you are a child')
+}else if(age >= 13 && age <= 19){
+  console.log('you are a teenager')
+}else (age >= 20)
+  console.log('you are an adult')
